b84387ca8eef4d36acadd43beed4ce649635c196
noah.fedele@student.htl-rankweil.at <noah.fedele@student.htl-rankweil.at>
2024-10-16 10:29:25 +0000

    Created HomeForm.RowTemplate1; Edited settings, HomeForm and SaveAndSendEmail

---
+++ client_code/HomeForm/__init__.py
@@ -0,0 +1,29 @@
+from ._anvil_designer import HomeFormTemplate
+from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+
+class HomeForm(HomeFormTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
+
+    self.drop_down_1.items = [("Satteins", 0), ("Mordor", 1)]
+    print(anvil.server.call("say_hello", "sauron"))
+    print(anvil.server.call("get_jugendherbergen"))
+
+    self.drop_down_1.items = anvil.server.call('get_jugendherbergen', "name, JID")
+    
+
+  
+
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    print(self.drop_down_1.items[self.drop_down_1.selected_value - 1][1])
+    pass
+
--- server_code/SaveAndSendEmail.py
+++ server_code/SaveAndSendEmail.py
@@ -1,34 +1,33 @@
-import anvil.email
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.files
+from anvil.files import data_files
 import anvil.server
-from datetime import datetime
+import sqlite3
 
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+@anvil.server.callable
+def say_hello(name):
+  print("Hello, " + name + "!")
+  return 42
 
 @anvil.server.callable
-def add_feedback(name, email, feedback):
-  # This function is called from HomeForm when the user clicks "SUBMIT".
-
-  # We use Anvil's built-in Data Tables to save the feedback.
-  # You can find your Data Tables in the navigation on the left.
-  app_tables.feedback.add_row(
-    name=name, email=email, feedback=feedback, created=datetime.now()
-  )
-
-  # Now we send an email to you, the app's author!
-  anvil.email.send(  # to="somebody@example.com", # Change this to your email address
-    subject="Feedback from {}".format(name),
-    text="""
-    A new person has filled out the feedback form!
-    
-    Name: {}
-    Email address: {}
-    Feedback:
-    {}
-    """.format(name, email, feedback),
-  )
-
-  # Pretty simple, huh?
-  # To learn more about Anvil, go to:
-  # https://anvil.works/learn/tutorials
+def get_jugendherbergen(rows="*"):
+  conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(f"SELECT {rows} FROM jugendherbergen"))
+  return res
+@anvil.server.callable
+def get_zimmer(jid,rows="*"):
+  conn = sqlite3.connect(data_files['jugendherbergen_verwaltung.db'])
+  cursor = conn.cursor()
+  res = list(cursor.execute(f"SELECT {rows} FROM zimmer WHERE JID = {jid}"))
+  conn.close()
+  return res
